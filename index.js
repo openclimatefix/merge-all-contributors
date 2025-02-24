5b43bbac10d3e14811d702c35ce1746aa2f7484d
aryan lamba <aryanlamba54@gmail.com>
2025-02-24 14:23:29 +0530

    formatted index.js

--- index.js
+++ index.js
@@ -1,103 +1,114 @@
 #!/usr/bin/env node
 
-const axios = require('axios');
-const core = require('@actions/core');
-
+const axios = require("axios");
+const core = require("@actions/core");
 
 async function getReposForPage(page, org) {
-    console.log(`Fetching page ${page} of repos for ${org}`)
-    const {
-        status,
-        data
-    } = await axios.get(`https://api.github.com/orgs/${org}/repos`, {
-            params: {
-                per_page: 100,
-                page: page }
-        });
-    if (status !== 200) {
-        throw err;
-    }
+  console.log(`Fetching page ${page} of repos for ${org}`);
+  const { status, data } = await axios.get(
+    `https://api.github.com/orgs/${org}/repos`,
+    {
+      params: {
+        per_page: 100,
+        page: page,
+      },
+    },
+  );
+  if (status !== 200) {
+    throw err;
+  }
 
-    return data;
+  return data;
 }
 
 async function getAllReposForOrg(org) {
-    let i = 1;
-    let allRepos = [];
+  let i = 1;
+  let allRepos = [];
 
-    while (i<10) {
-        const repos = await getReposForPage(i, org)
-        allRepos = allRepos.concat(repos)
+  while (i < 10) {
+    const repos = await getReposForPage(i, org);
+    allRepos = allRepos.concat(repos);
 
-        console.log(`Fetched ${repos.length} repos for ${org}`)
-        if (repos.length !== 100) {
-            break
-        }
-        i=i+1;
-
-        if (i == 10) {
-            throw `More than 1000 repos in ${org}, not supported`
-        }
+    console.log(`Fetched ${repos.length} repos for ${org}`);
+    if (repos.length !== 100) {
+      break;
     }
+    i = i + 1;
 
-    return allRepos;
+    if (i == 10) {
+      throw `More than 1000 repos in ${org}, not supported`;
+    }
+  }
+
+  return allRepos;
 }
 
 async function getAllContributorsForRepo(repo_handle) {
-    const {
-        status,
-        data
-    } = await axios.get(`https://raw.githubusercontent.com/${repo_handle}/main/.all-contributorsrc`, {
-        validateStatus: false,
-    });
+  const { status, data } = await axios.get(
+    `https://raw.githubusercontent.com/${repo_handle}/main/.all-contributorsrc`,
+    {
+      validateStatus: false,
+    },
+  );
 
-    if (status === 404) {
-        return;
-    } else if (status !== 200) {
-        throw `Could not fetch all-contributors for repo ${repo_handle}`
-    }
+  if (status === 404) {
+    return;
+  } else if (status !== 200) {
+    throw `Could not fetch all-contributors for repo ${repo_handle}`;
+  }
 
-    return data;
+  return data;
 }
 
 async function main(org) {
-    const allRepos = await getAllReposForOrg(org);
+  const allRepos = await getAllReposForOrg(org);
 
-    const getAllContributorsPromises = allRepos.map(repo => getAllContributorsForRepo(repo.full_name))
-    Promise.all(getAllContributorsPromises).then(allContributorFiles => {
-        const allContributorsDict = allContributorFiles.reduce((result, contributorFile) => {
-            if (contributorFile != null) {
-                for (contributor of contributorFile.contributors) {
-                    if (contributor.profile in result) {
-                        for (contributionType of contributor.contributions) {
-                            if (result[contributor.profile].contributions.indexOf(contributionType) === -1) {
-                                result[contributor.profile].contributions.push(contributionType)
-                            }
-                        }
-                    } else {
-                        result[contributor.profile] = contributor
-                    }
+  const getAllContributorsPromises = allRepos.map((repo) =>
+    getAllContributorsForRepo(repo.full_name),
+  );
+  Promise.all(getAllContributorsPromises).then((allContributorFiles) => {
+    const allContributorsDict = allContributorFiles.reduce(
+      (result, contributorFile) => {
+        if (contributorFile != null) {
+          for (contributor of contributorFile.contributors) {
+            if (contributor.profile in result) {
+              for (contributionType of contributor.contributions) {
+                if (
+                  result[contributor.profile].contributions.indexOf(
+                    contributionType,
+                  ) === -1
+                ) {
+                  result[contributor.profile].contributions.push(
+                    contributionType,
+                  );
                 }
+              }
+            } else {
+              result[contributor.profile] = contributor;
             }
-            return result;
-        }, {});
-
-        const allContributors = Object.values(allContributorsDict);
-        console.log(allContributors)
-
-        if (IS_GH_ACTION) {
-            core.setOutput("contributors", JSON.stringify(allContributors));
+          }
         }
-    })
+        return result;
+      },
+      {},
+    );
+
+    const allContributors = Object.values(allContributorsDict);
+    console.log(allContributors);
+
+    if (IS_GH_ACTION) {
+      core.setOutput("contributors", JSON.stringify(allContributors));
+    }
+  });
 }
 
 let IS_GH_ACTION = process.argv.length < 3;
 
-if (IS_GH_ACTION && !core.getInput('orgName')) {
-    console.log("[ERROR] Must pass GitHub organisation name");
-    console.log("Either pass to CLI or set as input to GH Action")
-    console.log("Example: merge-all-contributors openclimatefix");
-    return;
+if (IS_GH_ACTION && !core.getInput("orgName")) {
+  console.log("[ERROR] Must pass GitHub organisation name");
+  console.log("Either pass to CLI or set as input to GH Action");
+  console.log("Example: merge-all-contributors openclimatefix");
+  return;
 }
 
-main(core.getInput('orgName') || process.argv[2])
+main(core.getInput("orgName") || process.argv[2]);
